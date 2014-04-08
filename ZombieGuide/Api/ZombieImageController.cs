787a68618a4cf79ca54e1b1dea7791a376dd0256
Josh Carroll <jwarren.carroll@gmail.com>
2014-04-08 01:26:06 -0400

    Added simple API controller

---
+++ ZombieGuide/Api/ZombieImageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace ZombieGuide.Api
+{
+    public class ZombieImageController : ApiController
+    {
+        public IEnumerable<String> GetZombieImages()
+        {
+            var filePath = HttpContext.Current.Server.MapPath("~/content/zombies/");
+            var dirInfo = new DirectoryInfo(filePath);
+
+            return dirInfo.EnumerateFiles().Select(fi => fi.Name);
+        }
+    }
+}
