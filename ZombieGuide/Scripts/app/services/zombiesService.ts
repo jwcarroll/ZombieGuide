25a2882e5ffc2778856b79e373a78ec881873e99
Josh Carroll <jwarren.carroll@gmail.com>
2014-04-08 02:09:46 -0400

    simple service using TypeScript

---
+++ ZombieGuide/Scripts/app/services/zombiesService.ts
@@ -0,0 +1,29 @@
+﻿interface IZombie {
+    id?: number;
+    name: string;
+    notes: string;
+    imageName?: string;
+}
+
+class ZombiesService {
+    public getZombies() {
+        $.ajax('/api/zombies', {
+            type: 'GET'
+        }).then(result => {
+            alert(JSON.stringify(result));
+        });
+    }
+
+    public createZombie(zombie: IZombie) {
+        $.ajax('/api/zombies', {
+            type: 'POST',
+            data: zombie
+        }).then(result => {
+            alert(JSON.stringify(result));
+        });
+    }
+}
+
+var service = new ZombiesService();
+
+service.createZombie({ name: 'Fred', notes: 'He will eat your face!' });
