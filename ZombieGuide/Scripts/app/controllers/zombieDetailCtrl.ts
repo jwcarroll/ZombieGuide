39a33aa2234a29bfd3e483cd1bd6c8a750b14507
Josh Carroll <jwarren.carroll@gmail.com>
2014-04-08 02:35:24 -0400

    Initial detail controller

---
+++ ZombieGuide/Scripts/app/controllers/zombieDetailCtrl.ts
@@ -0,0 +1,37 @@
+﻿module Controllers {
+
+    class ZombieDetailController {
+        public static $inject = ['$location', 'zombieService'];
+
+        public zombie: Services.IZombie;
+        public imageNames: string[];
+
+        constructor(
+            private $location: ng.ILocationService,
+            private zombieService: Services.ZombieService) {
+            this.init();
+        }
+
+        private init() {
+            this.zombieService.getZombieImageNames()
+                .success(names => {
+                    this.imageNames = names;
+                });
+
+            this.zombie = <Services.IZombie>{};
+        }
+
+        public save() {
+            this.saveCurrentZombie()
+                .success(z => {
+                    this.$location.path('/zombies');
+                });
+        }
+
+        private saveCurrentZombie(): ng.IHttpPromise<Services.IZombie> {
+            return this.zombieService.createZombie(this.zombie);
+        }
+    }
+
+    Application.Module.controller('ZombieDetailController', ZombieDetailController);
+} 
